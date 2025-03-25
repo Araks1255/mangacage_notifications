86f17afd69f9b4bb64c07bae144525278e1ca219
Araks <lebedevartemiy74@gmail.com>
2025-03-25 17:24:18 +0500

    переделал все уведомления (теперь при приходе чего-то нового и чего-то отредактированного выводятся разные сообщения), починил уведомления о выходе новых глав в тайтле, на который подписаны юзеры

--- internal/grpc/service_notifications.go
+++ internal/grpc/service_notifications.go
@@ -8,33 +8,99 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func (s Server) NotifyAboutNewTitleOnModeration(ctx context.Context, title *pb.TitleOnModeration) (*pb.Empty, error) {
+func (s Server) NotifyAboutTitleOnModeration(ctx context.Context, title *pb.TitleOnModeration) (*pb.Empty, error) {
 	var allowedUsersTgIds []int64
-	db.Raw("SELECT users.tg_user_id FROM users INNER JOIN user_roles ON users.id = user_roles.user_id INNER JOIN roles ON user_roles.role_id = roles.id WHERE roles.name = 'moder' OR roles.name = 'admin'").Scan(&allowedUsersTgIds)
+	db.Raw(`SELECT users.tg_user_id FROM users
+		INNER JOIN user_roles ON users.id = user_roles.user_id
+		INNER JOIN roles ON user_roles.role_id = roles.id
+		WHERE roles.name = 'moder' OR roles.name = 'admin'`,
+	).Scan(&allowedUsersTgIds)
 
 	var msg tgbotapi.MessageConfig
+
+	var response string
+	if title.New {
+		response = fmt.Sprintf("На модерацию пришёл новый тайтл\n\nНазвание: %s", title.Name)
+	} else {
+		response = fmt.Sprintf("На модерацию пришли изменения для тайтла %s", title.Name)
+	}
+
 	for i := 0; i < len(allowedUsersTgIds); i++ {
-		msg = tgbotapi.NewMessage(allowedUsersTgIds[i], fmt.Sprintf("На модерацию пришёл новый тайтл\n\nНазвание: %s", title.TitleName))
-		_, err := serviceBot.Send(msg)
-		if err != nil {
-			return &pb.Empty{}, err
-		}
+		msg = tgbotapi.NewMessage(allowedUsersTgIds[i], response)
+		serviceBot.Send(msg)
 	}
 
 	return &pb.Empty{}, nil
 }
 
-func (s Server) NotifyAboutNewChapterOnModeration(ctx context.Context, chapter *pb.ChapterOnModeration) (*pb.Empty, error) {
+func (s Server) NotifyAboutVolumeOnModeration(ctx context.Context, volume *pb.VolumeOnModeration) (*pb.Empty, error) {
 	var allowedUsersTgIds []int64
-	db.Raw("SELECT users.tg_user_id FROM users INNER JOIN user_roles ON users.id = user_roles.user_id INNER JOIN roles ON user_roles.role_id = roles.id WHERE roles.name = 'moder' OR roles.name = 'admin'").Scan(&allowedUsersTgIds)
+	db.Raw(`SELECT users.tg_user_id FROM users
+		INNER JOIN user_roles ON users.id = user_roles.user_id
+		INNER JOIN roles ON user_roles.role_id = roles.id
+		WHERE roles.name = 'moder' OR roles.name = 'admin'`,
+	).Scan(&allowedUsersTgIds)
 
 	var msg tgbotapi.MessageConfig
+
+	var response string
+	if volume.New {
+		response = fmt.Sprintf("На модерацию пришёл новый том\n\nНазвание: %s", volume.Name)
+	} else {
+		response = fmt.Sprintf("На модерацию пришли изменения для тома %s", volume.Name)
+	}
+
 	for i := 0; i < len(allowedUsersTgIds); i++ {
-		msg = tgbotapi.NewMessage(allowedUsersTgIds[i], fmt.Sprintf("На модерацию пришла новая глава для тайтла:\n%s\n\nНазвание:\n%s", chapter.TitleName, chapter.ChapterName))
-		_, err := serviceBot.Send(msg)
-		if err != nil {
-			return &pb.Empty{}, err
-		}
+		msg = tgbotapi.NewMessage(allowedUsersTgIds[i], response)
+		serviceBot.Send(msg)
+	}
+
+	return &pb.Empty{}, nil
+}
+
+func (s Server) NotifyAboutChapterOnModeration(ctx context.Context, chapter *pb.ChapterOnModeration) (*pb.Empty, error) {
+	var allowedUsersTgIds []int64
+	db.Raw(`SELECT users.tg_user_id FROM users
+		INNER JOIN user_roles ON users.id = user_roles.user_id
+		INNER JOIN roles ON user_roles.role_id = roles.id
+		WHERE roles.name = 'moder' OR roles.name = 'admin'`,
+	).Scan(&allowedUsersTgIds)
+
+	var msg tgbotapi.MessageConfig
+
+	var response string
+	if chapter.New {
+		response = fmt.Sprintf("На модерацию пришла новая глава\n\nНазвание: %s", chapter.Name)
+	} else {
+		response = fmt.Sprintf("На модерацию пришли изменения для главы %s", chapter.Name)
+	}
+	for i := 0; i < len(allowedUsersTgIds); i++ {
+		msg = tgbotapi.NewMessage(allowedUsersTgIds[i], response)
+		serviceBot.Send(msg)
+	}
+
+	return &pb.Empty{}, nil
+}
+
+func (s Server) NotifyAboutUserOnModeration(ctx context.Context, user *pb.User) (*pb.Empty, error) {
+	var allowedUsersTgIds []int64
+	db.Raw(`SELECT users.tg_user_id FROM users
+		INNER JOIN user_roles ON users.id = user_roles.user_id
+		INNER JOIN roles ON user_roles.role_id = roles.id
+		WHERE roles.name = 'moder' OR roles.name = 'admin'`,
+	).Scan(&allowedUsersTgIds)
+
+	var msg tgbotapi.MessageConfig
+
+	var response string
+	if user.New {
+		response = fmt.Sprintf("Верификации ожидает новый пользователь\n\nИмя: %s", user.Name)
+	} else {
+		response = fmt.Sprintf("Подтверждения изменений аккаунта ожидает пользователь %s", user.Name)
+	}
+	for i := 0; i < len(allowedUsersTgIds); i++ {
+		msg = tgbotapi.NewMessage(allowedUsersTgIds[i], response)
+		serviceBot.Send(msg)
 	}
 
 	return &pb.Empty{}, nil
--- internal/grpc/users_notifications.go
+++ internal/grpc/users_notifications.go
@@ -2,66 +2,80 @@ package grpc
 
 import (
 	"context"
-	"errors"
+	//"errors"
 	"fmt"
-	"log"
+	//"log"
 
-	promoUtils "github.com/Araks1255/mangacage_promocodes/pkg/common/utils"
+	//promoUtils "github.com/Araks1255/mangacage_promocodes/pkg/common/utils"
 	pb "github.com/Araks1255/mangacage_protos"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/spf13/viper"
+	//"github.com/spf13/viper"
 )
 
 func (s Server) NotifyAboutReleaseOfNewChapterInTitle(ctx context.Context, chapter *pb.ReleasedChapter) (*pb.Empty, error) {
-	var chapterTitleID uint
-	db.Raw("SELECT title_id FROM chapters WHERE name = ?", chapter.Name).Scan(&chapterTitleID)
+	var inf struct {
+		Chapter              string
+		Title                string
+		Volume               string
+		SubscribedUsersTgIDs []int64
+	}
 
-	var chapterTitleName string
-	db.Raw("SELECT name FROM titles WHERE id = ?", chapterTitleID).Scan(&chapterTitleName)
-
-	var subscribedUsersIDs []int64
-	db.Raw("SELECT users.tg_user_id FROM users INNER JOIN user_titles_subscribed_to ON users.id = user_titles_subscribed_to.user_id WHERE user_titles_subscribed_to.title_id = ?", chapterTitleID).Scan(&subscribedUsersIDs)
+	db.Raw(
+		`SELECT c.name AS chapter, volumes.name AS volume, titles.name AS title,
+		(
+			SELECT ARRAY (
+				SELECT users.tg_user_id FROM users
+				INNER JOIN user_titles_subscribed_to ON users.id = user_titles_subscribed_to.user_id
+				INNER JOIN titles ON titles.id = user_titles_subscribed_to.title_id
+				INNER JOIN volumes ON volumes.title_id = titles.id
+				INNER JOIN chapters ON chapters.volume_id = volumes.id
+				WHERE chapters.id = c.id
+			) AS subscribed_users_tg_ids
+		) FROM chapters AS c
+		 INNER JOIN volumes ON c.volume_id = volumes.id
+		 INNER JOIN titles ON volumes.title_id = titles.id
+		 WHERE c.id = ?`, chapter.ID,
+	).Scan(&inf)
 
 	var msg tgbotapi.MessageConfig
-	response := fmt.Sprintf("Вышла новая глава в тайтле %s\n\nНазвание - %s", chapterTitleName, chapter.Name)
-	for i := 0; i < len(subscribedUsersIDs); i++ {
-		msg = tgbotapi.NewMessage(subscribedUsersIDs[i], response)
-		if _, err := usersBot.Send(msg); err != nil {
-			return &pb.Empty{}, err
-		}
+	response := fmt.Sprintf("Вышла новая глава в тайтле %s\n\nНазвание - %s", inf.Title, inf.Chapter)
+
+	for i := 0; i < len(inf.SubscribedUsersTgIDs); i++ {
+		msg = tgbotapi.NewMessage(inf.SubscribedUsersTgIDs[i], response)
+		usersBot.Send(msg)
 	}
 
 	return &pb.Empty{}, nil
 }
 
-func (s Server) SendPromocode(ctx context.Context, promocodeRequest *pb.PromocodeRequest) (*pb.Empty, error) {
-	viper.SetConfigFile("./pkg/common/envs/.env")
-	viper.ReadInConfig()
+// func (s Server) SendPromocode(ctx context.Context, promocodeRequest *pb.PromocodeRequest) (*pb.Empty, error) {
+// 	viper.SetConfigFile("./pkg/common/envs/.env")
+// 	viper.ReadInConfig()
 
-	key := []byte(viper.Get("PROMO_KEY").(string))
+// 	key := []byte(viper.Get("PROMO_KEY").(string))
 
-	var tgUserID int64
-	db.Raw("SELECT tg_user_id FROM users WHERE name = ?", promocodeRequest.User.Name).Scan(&tgUserID)
-	if tgUserID == 0 {
-		return &pb.Empty{}, errors.New("Пользователь не найден")
-	}
+// 	var tgUserID int64
+// 	db.Raw("SELECT tg_user_id FROM users WHERE name = ?", promocodeRequest.User.Name).Scan(&tgUserID)
+// 	if tgUserID == 0 {
+// 		return &pb.Empty{}, errors.New("Пользователь не найден")
+// 	}
 
-	decryptedCode, err := promoUtils.DecryptPromocode(promocodeRequest.Promocode.Code, key)
-	if err != nil {
-		log.Println(err)
-		return &pb.Empty{}, err
-	}
+// 	decryptedCode, err := promoUtils.DecryptPromocode(promocodeRequest.Promocode.Code, key)
+// 	if err != nil {
+// 		log.Println(err)
+// 		return &pb.Empty{}, err
+// 	}
 
-	msg := tgbotapi.NewMessage(tgUserID,
-		fmt.Sprintf("Здравствуйте %s!\n\nВот ваш промокод на %s алмазиков:\n%s",
-			promocodeRequest.User.Name,
-			promocodeRequest.Promocode.Amount,
-			decryptedCode))
+// 	msg := tgbotapi.NewMessage(tgUserID,
+// 		fmt.Sprintf("Здравствуйте %s!\n\nВот ваш промокод на %s алмазиков:\n%s",
+// 			promocodeRequest.User.Name,
+// 			promocodeRequest.Promocode.Amount,
+// 			decryptedCode))
 
-	if _, err = usersBot.Send(msg); err != nil {
-		log.Println(err)
-		return &pb.Empty{}, err
-	}
+// 	if _, err = usersBot.Send(msg); err != nil {
+// 		log.Println(err)
+// 		return &pb.Empty{}, err
+// 	}
 
-	return &pb.Empty{}, nil
-}
+// 	return &pb.Empty{}, nil
+// }
